1964071a1d8f727a220b360e4e03f39904e4c072
MaxGoroshkoTU <tug47632@temple.edu>
2019-10-27 17:39:14 -0400

    BookListFragment finished

---
+++ app/src/main/java/edu/temple/bookcase/BookListFragment.java
@@ -0,0 +1,99 @@
+package edu.temple.bookcase;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class BookListFragment extends Fragment {
+
+
+    public static final String BOOK_NAME = "books";
+    ArrayList<String> books;
+
+
+    private OnFragmentInteractionListener fragmentParent;
+
+    public BookListFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static BookListFragment newInstance(ArrayList<String> books) {
+        BookListFragment bookListFragment = new BookListFragment();
+        Bundle args = new Bundle();
+        args.putStringArrayList(BOOK_NAME,books);
+        bookListFragment.setArguments(args);
+        return bookListFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            books = getArguments().getStringArrayList(BOOK_NAME);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ListView listView = (ListView) inflater.inflate(R.layout.fragment_book_list,container,false);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                fragmentParent.onFragmentInteraction(position);
+            }
+        });
+        return listView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(int position) {
+        if (fragmentParent != null) {
+            fragmentParent.onFragmentInteraction(position);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            fragmentParent = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        fragmentParent = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(int position);
+    }
+}
