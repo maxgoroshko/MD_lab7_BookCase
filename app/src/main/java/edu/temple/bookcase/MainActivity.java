178acde0c9af8eb803fdba5aad0d1fb9e5c3fda4
MaxGoroshkoTU <tug47632@temple.edu>
2019-10-27 16:27:49 -0400

    Test Books added

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -2,13 +2,27 @@ package edu.temple.bookcase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        books = Arrays.asList(res.getStringArray(R.array.books));
+
     }
 }
